4dfe7ab8b1d5dedc5bf9ce7de639a60430a41879
hardi shah <hardishahhardi@gmail.com>
2021-02-04 21:05:57 +0530

    Initial Commit

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -8,16 +8,3 @@ function Input(props) {
     )
 }
 export default Input
-
-// import React, { FC } from 'react'
-// interface TagProps {
-//      title?: string;
-// }
-// const Tag: FC<TagProps> = ({ title="My title" }) => {
-//      return (
-//          <div style={{backgroundColor: 'yellow'}}>
-//              {title}
-//          </div>
-//      )
-// }
-// export default Tag
